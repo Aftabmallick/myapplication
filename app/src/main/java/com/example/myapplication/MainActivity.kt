f487fe221baf0a7857d0a3d805455ffcd5418928
Aftab Mallick <78075420+Aftabmallick@users.noreply.github.com>
2023-03-06 20:00:35 +0530

    modified

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import android.content.Intent as Intent
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,9 +18,11 @@ class MainActivity : AppCompatActivity() {
         val greetingTextView = findViewById<TextView>(R.id.tvHello)
         val inputField = findViewById<EditText>(R.id.tvName)
         val submitButton = findViewById<Button>(R.id.btnSubmit)
+        val offersButton= findViewById<Button>(R.id.btnOffers)
         submitButton.setOnClickListener{
             val enteredName = inputField.text.toString()
             if(enteredName==""){
+                offersButton.visibility= INVISIBLE
                 greetingTextView.text=""
                 Toast.makeText(this@MainActivity,"Please Enter Your Name", Toast.LENGTH_SHORT).show()
             }
@@ -24,7 +30,13 @@ class MainActivity : AppCompatActivity() {
                 val message = "Welcome $enteredName"
                 greetingTextView.text = message
                 inputField.text.clear()
+                offersButton.visibility=VISIBLE
+
             }
+        }
+        offersButton.setOnClickListener{
+            val intent = Intent(this,Activity2::class.java)
+            startActivity(intent)
         }
     }
 }
